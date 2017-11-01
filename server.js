7f37eee45f928dfd07389c0ab0088f9ad8f37f9e
yangyh <136585913@qq.com>
2017-11-01 14:54:24 +0800

    修改结构

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var serverport = 8080;
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(serverport, function() {
-    console.log("server start " + serverport)
-})
